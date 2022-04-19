04dfd8a2d70ee62e2336a20e046d3072d8f6d33a
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-04-19 20:55:32 +0300

    add logic angular

--- frontend/angular-todo/src/app/todo-form/todo-form.component.ts
+++ frontend/angular-todo/src/app/todo-form/todo-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { TodoListService } from '../todo-list/todo-list.service';
 
 @Component({
   selector: 'app-todo-form',
@@ -7,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./todo-form.component.scss'],
 })
 export class TodoFormComponent implements OnInit {
+  @Input() refresh!: () => void;
   validateForm!: FormGroup;
 
   submitForm(value: { title: string; completed: boolean }): void {
@@ -17,10 +20,18 @@ export class TodoFormComponent implements OnInit {
       }
     }
     value.completed = false;
+    this.todoListService.create(value).subscribe(() => {
+      this.nzMessageService.info('Todo created');
+      this.refresh();
+    });
     this.validateForm.reset();
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private todoListService: TodoListService,
+    private nzMessageService: NzMessageService
+  ) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
