217008bd7610e844bd777eb762dcbd88a6ee31fd
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-04-19 18:26:33 +0300

    add backend | Nodejs

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Todo } from './todos/entities/todo.entity';
+import { TodosModule } from './todos/todos.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      // port: 54321,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestToDo',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/todos/todos.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { TodosController } from './todos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
+  controllers: [TodosController],
+  providers: [TodosService],
+})
+export class TodosModule {
+  static forRoot(): import("@nestjs/common").Type<any> | import("@nestjs/common").DynamicModule | Promise<import("@nestjs/common").DynamicModule> | import("@nestjs/common").ForwardReference<any> {
+    throw new Error('Method not implemented.');
+  }
+}
