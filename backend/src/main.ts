5a1f151fecdec6c742dbf64bed44f80412136f3d
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-04-21 15:01:20 +0300

    add cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+/** @format */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: 'http://localhost:4200',
+		credentials: true,
+	});
+	await app.listen(8000);
 }
 bootstrap();
